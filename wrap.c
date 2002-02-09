19de702da89d76c4151af3592e3b0f992782f1a5
till <>
2002-02-09 00:48:32 +0000

    - fixed compiler warnings about printf argument typing.  - added a #ifdef switch for either using the --wrap= ld option    or hardcoding the wrapper routine names.

--- wrap.c
+++ wrap.c
@@ -1,3 +1,4 @@
+/* Id */
 #include <fcntl.h>
 #include <stdio.h>
 #include <libelf/libelf.h>
@@ -15,6 +16,9 @@
  * NOTE: this is kind of a hack because it makes some assumptions
  *       about libelf internals. It should work with libelf-0.8.0.
  *
+ *       THIS WRAPPER SHOULD NOT BE USED WITH SOFTWARE OTHER THAN
+ *       LIBELF-0.8.0
+ *
  * Assumptions:
  *   elf_begin() makes one call to lseek(0,SEEK_END) to find out
  *   the file size and essentially reads the entire file
@@ -29,14 +33,31 @@
  * --> ftruncate allocates a buffer and zeroes it.
  */
 
+/* Author: Till Straumann <strauman@slac.stanford.edu> 2/2002 */
+/* License: GPL, see http://www.gnu.org for details */
+
 #define MAXBUFS 10
 #define BLOCKSZ 1000
 
 #define FLG_READ	1
 #define FLG_WRITE	2
 
+#ifdef LET_LD_DO_THE_WRAPPING
+/* use --wrap=lseek etc. when linking */
+#define my_lseek		__wrap_lseek
+#define my_mmap			__wrap_mmap
+#define my_munmap		__wrap_munmap
+#define my_ftruncate	__wrap_ftruncate
+#else
+/* hardcoded names, these must be changed in the libelf sources */
+#define my_lseek		libelf_lseek_hack
+#define my_mmap			libelf_mmap_hack
+#define my_munmap		libelf_munmap_hack
+#define my_ftruncate	libelf_ftruncate_hack
+#endif
+
 typedef struct hackbuf_ {
-	char 		*d;
+	char	 		*d;
 	unsigned long	s;
 	unsigned long	flags;
 } HackBuf;
@@ -48,7 +69,7 @@ __wrap_lseek(int fd, off_t off, int whence)
 {
 off_t	rval=0,got;
 
-	fprintf(stderr,"seek to %i\n",off);
+	fprintf(stderr,"LSEEK WRAP: seek to %li\n",(unsigned long)off);
 
 	assert(0==off);
 	assert(fd<MAXBUFS);
@@ -57,7 +78,7 @@ off_t	rval=0,got;
 		case SEEK_END:
 			assert(0==bufs[fd].d);
 			/* slurp in the file */
-			fprintf(stderr,"SLURPING on fd %i\n",fd);
+			fprintf(stderr,"LSEEK WRAP: slurping on fd %i into buffer until EOF\n",fd);
 			do {
 				assert(bufs[fd].d=realloc(bufs[fd].d,rval+BLOCKSZ));
 				got=read(fd,bufs[fd].d+rval,BLOCKSZ);
@@ -82,8 +103,8 @@ void *
 __wrap_mmap(void *start, size_t len, int prot, int flags, int fd, off_t offset)
 {
 	assert(0==start);
-	fprintf(stderr,"Mapping fd %i, offset 0x%08x, len %i\n",
-			fd, offset, len);
+	fprintf(stderr,"MMAP WRAP: Mapping fd %li, offset 0x%08lx, len %li\n",
+			(long)fd, (unsigned long)offset, (unsigned long)len);
 	assert(bufs[fd].d);
 	return (void*)(bufs[fd].d+offset);
 }
@@ -94,10 +115,10 @@ __wrap_munmap(void *start, size_t len)
 int i;
 	for (i=0; i<MAXBUFS; i++) {
 		if (bufs[i].d && (char *)start>=bufs[i].d && (char *)start<bufs[i].d+bufs[i].s) {
-			fprintf(stderr,"releasing buffer #%i (0x%08x, addr was 0x%08x)",
-					i, bufs[i], start);
+			fprintf(stderr,"MUNMAP WRAP: releasing buffer #%i (starts at %p, release addr was %p)",
+					i, bufs[i].d, start);
 			if (bufs[i].flags&FLG_WRITE) {
-				fprintf(stderr,"--writing back--");
+				fprintf(stderr,"--writing back to file--");
 				write(i,start,len);
 			}
 			fprintf(stderr,"\n");

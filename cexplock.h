ef98af99a1f679dcca64e9b3eb5de1b7908099c9
strauman <>
2002-09-07 22:33:49 +0000

    - implemented RW locks

--- cexplock.h
+++ cexplock.h
@@ -9,16 +9,27 @@
 #include <epicsMutex.h>
 
 typedef epicsMutexId CexpLock;
+typedef epicsEventId CexpEvent;
 
 static __inline__ int
 cexpLockCreate(CexpLock *lp)
 {
-	return (*lp=epicsMutexCreate()) ? 0 : -1;
+	return ! (*lp=epicsMutexCreate());
+}
+
+static __inline__ int
+cexpEventCreate(CexpEvent *id)
+{
+	return ! (*id=epicsEventCreate(EpicsEventEmpty));
 }
 
 #define cexpLock(l)   		epicsMutexLock((l))
 #define cexpUnlock(l) 		epicsMutexUnlock((l))
 #define cexpLockDestroy(l)	epicsMutexDestroy((l))
+
+#define cexpEventSend(e)	epicsEventSignal(e)
+#define cexpEventWait(e)	epicsEventWait(e)
+#define cexpEventDestroy(e)	epicsEventDestroy(e)
 
 
 #elif defined(__rtems)
@@ -34,21 +45,27 @@ long rtems_semaphore_destroy();
 
 #define rtems_build_name( _C1, _C2, _C3, _C4 ) \
   ( (_C1) << 24 | (_C2) << 16 | (_C3) << 8 | (_C4) )
-#define RTEMS_NO_TIMEOUT 		0
-#define RTEMS_WAIT		 		0
-#define RTEMS_PRIORITY			0x04	/* must be set to get priority inheritance */
-#define RTEMS_BINARY_SEMAPHORE	0x10
-#define RTEMS_INHERIT_PRIORITY	0x40
+#define RTEMS_NO_TIMEOUT 				0
+#define RTEMS_WAIT		 				0
+#define RTEMS_FIFO						0
+#define RTEMS_PRIORITY					0x04	/* must be set to get priority inheritance */
+#define RTEMS_BINARY_SEMAPHORE			0x10
+#define RTEMS_SIMPLE_BINARY_SEMAPHORE	0x20
+#define RTEMS_INHERIT_PRIORITY			0x40
 #endif
 
 typedef rtems_id CexpLock;
 
-#define cexpLock(l) 	rtems_semaphore_obtain((l), RTEMS_WAIT, RTEMS_NO_TIMEOUT)
-#define cexpUnlock(l)	rtems_semaphore_release((l))
+#define cexpLock(l) 		rtems_semaphore_obtain((l), RTEMS_WAIT, RTEMS_NO_TIMEOUT)
+#define cexpEventWait(l) 	rtems_semaphore_obtain((l), RTEMS_WAIT, RTEMS_NO_TIMEOUT)
+#define cexpUnlock(l)		rtems_semaphore_release((l))
+#define cexpEventSend(l)	rtems_semaphore_release((l))
+
 /* IMPORTANT: use a standard (not simple) binary semaphore that may nest */
-static inline void
+static inline int
 cexpLockCreate(CexpLock *l)
 {
+	return
 	rtems_semaphore_create(
 		rtems_build_name('c','e','x','p'),
 		1,/*initial count*/
@@ -57,7 +74,20 @@ cexpLockCreate(CexpLock *l)
 		l);
 }
 
+static inline int
+cexpEventCreate(CexpEvent *pe)
+{
+	return
+	rtems_semaphore_create(
+		rtems_build_name('c','e','x','p'),
+		0,/*initial count*/
+		RTEMS_SIMPLE_BINARY_SEMAPHORE,
+		0,
+		pe);
+}
+
 #define cexpLockDestroy(l) rtems_semaphore_delete((l))
+#define cexpEventDestroy(l) rtems_semaphore_delete((l))
 
 #elif defined(NO_THREAD_PROTECTION)
 typedef void *CexpLock;
@@ -68,5 +98,67 @@ typedef void *CexpLock;
 #else
 #error "thread protection not implemented for this target system"
 #endif
+
+typedef struct CexpRWLockRec_ {
+	CexpLock	mutex;
+	unsigned	readers;
+	CexpEvent	nap;
+	unsigned	sleeping_writers;
+} CexpRWLockRec, *CexpRWLock;
+
+static __inline__ void
+cexpRWLockInit(CexpRWLock pl)
+{
+	cexpLockCreate(&pl->mutex);
+	cexpEventCreate(&pl->nap);
+	pl->readers=0;
+	pl->sleeping_writers=0;
+}
+
+/* Readers / Writer lock implementation. This
+ * should be a little more efficient for the common
+ * case of a single reader which only has to 
+ * acquire one mutex.
+ * NOTE: A thread holding the write-lock may still
+ *       do nested readLock() readUnlock() calls.
+ *       However, a reader _MUST_NOT_ try to acquire
+ *       the write lock!
+ */
+static __inline__  void
+cexpReadLock(CexpRWLock l)
+{
+	cexpLock(l->mutex);
+	l->readers++;
+	cexpUnlock(l->mutex);
+}
+
+static __inline__ void
+cexpReadUnlock(CexpRWLock l)
+{
+	cexpLock(l->mutex);
+	if (0 == --l->readers && l->sleeping_writers) {
+		l->sleeping_writers--;
+		cexpEventSend(l->nap);
+	}
+	cexpUnlock(l->mutex);
+}
+
+static __inline__ void
+cexpWriteLock(CexpRWLock l)
+{
+	cexpLock(l->mutex);
+	while (l->readers) {
+		l->sleeping_writers++;
+		cexpUnlock(l->mutex);
+		cexpEventSleep(l->nap);
+		cexpLock(l->mutex);
+	}
+}
+
+static __inline__ void
+cexpWriteUnlock(CexpRWLock l)
+{
+	cexpUnlock(l->mutex);
+}
 
 #endif

c137e516d3f9ccb5a355a7cdd6f8f465a76d6d76
strauman <>
2008-04-03 21:51:38 +0000

    This commit was manufactured by cvs2svn to create tag 'R_pmbfd_orig'.
    Sprout from master 2008-04-03 21:51:37 UTC till ' - original import of poor man's ELF and BFD adapter library'
    Delete:
        ChangeLog
        INSTALL
        LICENSE
        Makefile
        Makefile.am
        Makefile.syms
        README
        bfd-disas.c
        bfdstuff.c
        cexp.c
        cexp.h
        cexp.y
        cexpHelp.h
        cexp_regex.h
        cexplock.h
        cexpmod.c
        cexpmod.h
        cexpmodP.h
        cexpsyms.c
        cexpsyms.h
        cexpsymsP.h
        configure.ac
        context.h
        cpptest/cpptest0.cc
        cpptest/cpptest1.cc
        ctyps.c
        ctyps.h
        elfsyms.c
        gencore.c
        gentab.c
        help.c
        noloader.c
        rtems-hackdefs.h
        teclastuff.c
        teclastuff.h
        vars.c
        vars.h
        wrap.c
        xsyms-bfd.c
        xsyms.c

--- noloader.c
+++
@@ -1,9 +0,0 @@
-#include <cexp.h>
-#include <cexpmodP.h>
-
-int
-cexpLoadFile(char *filename, CexpModule new_module)
-{
-	fprintf(stderr, "Unable to load '%s' -- no object loader was configured\n", filename);
-	return -1;
-}

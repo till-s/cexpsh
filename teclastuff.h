9e8a256cd85f2b2bf8b60c039052beb1fe91510a
strauman <>
2008-04-17 21:09:41 +0000

    - new LICENSE; we now use the SLAC license instead of the EPICS open    license.

--- teclastuff.h
+++ teclastuff.h
@@ -5,49 +5,57 @@
 /* Support for tecla features (such as word completion etc.) */
 
 /* LICENSING INFORMATION: note that the licensing terms for
- * using libtecla may be different from the EPICS open license
- * who applies to this file
+ * using libtecla may be different from the 'SLAC' license
+ * which applies to this file.
  */
 
-/*
- * Copyright 2002, Stanford University and
- * 		Till Straumann <strauman@slac.stanford.edu>
- * 
- * Stanford Notice
- * ***************
- * 
+/* SLAC Software Notices, Set 4 OTT.002a, 2004 FEB 03
+ *
+ * Authorship
+ * ----------
+ * This software (CEXP - C-expression interpreter and runtime
+ * object loader/linker) was created by
+ *
+ *    Till Straumann <strauman@slac.stanford.edu>, 2002-2008,
+ * 	  Stanford Linear Accelerator Center, Stanford University.
+ *
  * Acknowledgement of sponsorship
- * * * * * * * * * * * * * * * * *
- * This software was produced by the Stanford Linear Accelerator Center,
- * Stanford University, under Contract DE-AC03-76SFO0515 with the Department
- * of Energy.
+ * ------------------------------
+ * This software was produced by
+ *     the Stanford Linear Accelerator Center, Stanford University,
+ * 	   under Contract DE-AC03-76SFO0515 with the Department of Energy.
  * 
  * Government disclaimer of liability
- * - - - - - - - - - - - - - - - - -
+ * ----------------------------------
  * Neither the United States nor the United States Department of Energy,
- * nor any of their employees, makes any warranty, express or implied,
- * or assumes any legal liability or responsibility for the accuracy,
+ * nor any of their employees, makes any warranty, express or implied, or
+ * assumes any legal liability or responsibility for the accuracy,
  * completeness, or usefulness of any data, apparatus, product, or process
- * disclosed, or represents that its use would not infringe privately
- * owned rights.
+ * disclosed, or represents that its use would not infringe privately owned
+ * rights.
  * 
  * Stanford disclaimer of liability
- * - - - - - - - - - - - - - - - - -
+ * --------------------------------
  * Stanford University makes no representations or warranties, express or
  * implied, nor assumes any liability for the use of this software.
  * 
- * This product is subject to the EPICS open license
- * - - - - - - - - - - - - - - - - - - - - - - - - - 
- * Consult the LICENSE file or http://www.aps.anl.gov/epics/license/open.php
- * for more information.
+ * Stanford disclaimer of copyright
+ * --------------------------------
+ * Stanford University, owner of the copyright, hereby disclaims its
+ * copyright and all other rights in this software.  Hence, anyone may
+ * freely use it for any purpose without restriction.  
  * 
- * Maintenance of notice
- * - - - - - - - - - - -
+ * Maintenance of notices
+ * ----------------------
  * In the interest of clarity regarding the origin and status of this
- * software, Stanford University requests that any recipient of it maintain
- * this notice affixed to any distribution by the recipient that contains a
- * copy or derivative of this software.
- */
+ * SLAC software, this and all the preceding Stanford University notices
+ * are to remain affixed to any copy or derivative of this software made
+ * or distributed by the recipient and are to be affixed to any copy of
+ * software made or distributed by the recipient that contains a copy or
+ * derivative of this software.
+ * 
+ * SLAC Software Notices, Set 4 OTT.002a, 2004 FEB 03
+ */ 
 
 #include <libtecla.h>
 
--- wrap.c
+++ wrap.c
@@ -32,47 +32,54 @@
  * --> ftruncate allocates a buffer and zeroes it.
  */
 
-/* Author: Till Straumann <strauman@slac.stanford.edu> 2/2002 */
-
-/*
- * Copyright 2002, Stanford University and
- * 		Till Straumann <strauman@slac.stanford.edu>
- * 
- * Stanford Notice
- * ***************
- * 
+/* SLAC Software Notices, Set 4 OTT.002a, 2004 FEB 03
+ *
+ * Authorship
+ * ----------
+ * This software (CEXP - C-expression interpreter and runtime
+ * object loader/linker) was created by
+ *
+ *    Till Straumann <strauman@slac.stanford.edu>, 2002-2008,
+ * 	  Stanford Linear Accelerator Center, Stanford University.
+ *
  * Acknowledgement of sponsorship
- * * * * * * * * * * * * * * * * *
- * This software was produced by the Stanford Linear Accelerator Center,
- * Stanford University, under Contract DE-AC03-76SFO0515 with the Department
- * of Energy.
+ * ------------------------------
+ * This software was produced by
+ *     the Stanford Linear Accelerator Center, Stanford University,
+ * 	   under Contract DE-AC03-76SFO0515 with the Department of Energy.
  * 
  * Government disclaimer of liability
- * - - - - - - - - - - - - - - - - -
+ * ----------------------------------
  * Neither the United States nor the United States Department of Energy,
- * nor any of their employees, makes any warranty, express or implied,
- * or assumes any legal liability or responsibility for the accuracy,
+ * nor any of their employees, makes any warranty, express or implied, or
+ * assumes any legal liability or responsibility for the accuracy,
  * completeness, or usefulness of any data, apparatus, product, or process
- * disclosed, or represents that its use would not infringe privately
- * owned rights.
+ * disclosed, or represents that its use would not infringe privately owned
+ * rights.
  * 
  * Stanford disclaimer of liability
- * - - - - - - - - - - - - - - - - -
+ * --------------------------------
  * Stanford University makes no representations or warranties, express or
  * implied, nor assumes any liability for the use of this software.
  * 
- * This product is subject to the EPICS open license
- * - - - - - - - - - - - - - - - - - - - - - - - - - 
- * Consult the LICENSE file or http://www.aps.anl.gov/epics/license/open.php
- * for more information.
+ * Stanford disclaimer of copyright
+ * --------------------------------
+ * Stanford University, owner of the copyright, hereby disclaims its
+ * copyright and all other rights in this software.  Hence, anyone may
+ * freely use it for any purpose without restriction.  
  * 
- * Maintenance of notice
- * - - - - - - - - - - -
+ * Maintenance of notices
+ * ----------------------
  * In the interest of clarity regarding the origin and status of this
- * software, Stanford University requests that any recipient of it maintain
- * this notice affixed to any distribution by the recipient that contains a
- * copy or derivative of this software.
- */
+ * SLAC software, this and all the preceding Stanford University notices
+ * are to remain affixed to any copy or derivative of this software made
+ * or distributed by the recipient and are to be affixed to any copy of
+ * software made or distributed by the recipient that contains a copy or
+ * derivative of this software.
+ * 
+ * SLAC Software Notices, Set 4 OTT.002a, 2004 FEB 03
+ */ 
+
 #define MAXBUFS 10
 #define BLOCKSZ 1000
 

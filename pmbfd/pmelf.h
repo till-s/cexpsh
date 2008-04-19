7b3c0a15c1dcf706e93d2f0a60355afad24bdfd5
guest <>
2008-04-19 06:44:27 +0000

    - added definitions for x86_64 relocation types

--- pmbfd/pmelf.h
+++ pmbfd/pmelf.h
@@ -390,6 +390,41 @@ typedef union {
 #define R_PPC_SECTOFF_HA         36
 #define R_PPC_ADDR30             37
 
+/*
+ * SVR4 relocation types for x86_64
+ */
+#define R_X86_64_NONE             0
+#define R_X86_64_64               1
+#define R_X86_64_PC32             2
+#define R_X86_64_GOT32            3
+#define R_X86_64_PLT32            4
+#define R_X86_64_COPY             5
+#define R_X86_64_GLOB_DAT         6
+#define R_X86_64_JUMP_SLOT        7
+#define R_X86_64_RELATIVE         8
+#define R_X86_64_GOTPCREL         9
+#define R_X86_64_32              10
+#define R_X86_64_32S             11
+#define R_X86_64_16              12
+#define R_X86_64_PC16            13
+#define R_X86_64_8               14
+#define R_X86_64_PC8             15
+#define R_X86_64_DTPMOD64        16
+#define R_X86_64_DTPOFF64        17
+#define R_X86_64_TPOFF64         18
+#define R_X86_64_TLSGD           19
+#define R_X86_64_TLSLD           20
+#define R_X86_64_DTPOFF32        21
+#define R_X86_64_GOTTPOFF        22
+#define R_X86_64_TPOFF32         23
+#define R_X86_64_PC64            24
+#define R_X86_64_GOTOFF64        25
+#define R_X86_64_GOTPC32         26
+#define R_X86_64_SIZE32          32
+#define R_X86_64_SIZE64          33
+#define R_X86_64_GOTPC32_TLSDESC 34
+#define R_X86_64_TLSDESC_CALL    35
+#define R_X86_64_TLSDESC         36
 
 #define ELF32_R_SYM(x)		((x) >> 8)
 #define ELF32_R_TYPE(x) 	((uint8_t)((x)&0xff))

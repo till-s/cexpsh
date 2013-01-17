32ff3bdc778e0e5b74a4ac69db519e704b6434b5
strauman <>
2013-01-17 22:26:57 +0000

    - cexpsegs-powerpc-rtems.c: silenced compiler warnings.

--- cexpsegs-powerpc-rtems.c
+++ cexpsegs-powerpc-rtems.c
@@ -104,6 +104,7 @@
  */ 
 
 #include <rtems.h>
+#include <rtems/error.h>
 #include <stdlib.h>
 
 #ifdef HAVE_CONFIG_H
@@ -263,8 +264,7 @@ CexpSegment a;
 
 #ifdef CEXP_TEXT_REGION_SIZE
 rtems_status_code sc;
-rtems_id          id;
-unsigned long     start, start_unaligned;
+unsigned long     start, start_unaligned = LIMIT_32M;
 unsigned long     sz;
 #endif
 
